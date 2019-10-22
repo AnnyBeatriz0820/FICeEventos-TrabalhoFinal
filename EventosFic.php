31d592036be6813f3ee638ec9104fc22c2997e6c
rogeriols79 <rogerios2017@hotmail.com>
2019-10-22 17:54:17 -0200

    Tela para cadastro de login e senha e tela de login

---
+++ EventosFic.php
@@ -0,0 +1,33 @@
+<HTML>
+<HEAD>
+ <META charset="UTF-8">
+<TITLE>
+cadastro para Eventos e FIC
+</TITLE>
+</HEAD>
+<P><BODY STYLE="background-color:White";>
+<H2 STYLE="text-align:center;"> <FONT color="green very strong"> <B>EVENTOS E FIC</B></FONT></H2>
+<H2 STYLE="text-align:center;"> <FONT color="red"> <B> INSCRIÇÕES</B></FONT></H2>
+
+<div>
+<a href="http://localhost/cadastroPF.php"><H2 STYLE="text-align:center;"><a href ="Login.php"> <H2 estilo="font-size:80px;text-align:center;cor:preto">Cadastrar Login e Senha</H2 ><B><FONT color="Purple">
+
+<a href="http://localhost/FazerLogin.php"><H2 STYLE="text-align:center;"><a href ="FazerLogin.php"> <H2 estilo="font-size:80px;text-align:center;cor:preto">Fazer Login</H2 ><B><FONT color="Purple">
+<div >
+<div>
+	<a href="http://localhost/cadastroPF.php"> <IMG SRC="http://santaclaraecoresort.com.br/images/eventos-h3.png"></B></font></a>
+
+	
+<a href="http://localhost/cadastroPF.php"> <IMG SRC="http://ead.ifb.edu.br/images/imagens_menu/menu_lateral/2018-modalidade-fic.PNG"></B></H1></font></a>
+<div >
+
+
+<a href="https://ifce.edu.br/acopiara">
+<h1 style="text-align:center;"><FONT color="red"><B>acesse o site IFCE Acopiara</B></h1></FONT></a>    
+<a href="https://ifce.edu.br/acopiara">
+</FONT>
+</BODY>
+</div>
+</div>
+</HTML>
+
---
+++ cadastroPF.php
@@ -0,0 +1,98 @@
+<html>
+<body>
+  </div>
+       <h1> Cadastro do Estudante </h1>
+     
+      <div>
+
+<br/>
+<form method="post" action="Registro.php">
+   Curso: <select name="Curso" >
+
+    <option value="Ingles">Ingles</option>
+    <option value="Libras">Libras</option>
+    <option value="Matematica">Matematica</option>
+    <option value="Espanhol">Espanhol</option>
+    <option value="Programacao">Programaçao</option>
+    <option value="Introdução a Computacao">Introdução a Computacao</option>
+    <option value="Meio Ambiente">Meio Ambiente</option>
+    <option value="es">Informatica</option>
+     
+</select>
+ Matrícula: <input type="text" name="Nome" />
+ <br/>
+  <br/>
+  Nome Completo: <input type="text" name="Nome" />
+ CPF: <input type="text" name="Nome" />
+<br/>
+ <br/>
+ Telefone: <input type="tel" name="Telefone"/>
+
+   Sexo:
+   <input type="radio" name="Sexo" value="Masculino">M
+   <input type="radio" name="Sexo" value="Feminino">F
+   <input type="radio" name="Sexo" value="Outros">Outros
+    <br/>
+    <br/>
+    Data de Nascimento: <input type="date" name="data"/>
+   
+   Email: <input type="text" name="Email"/>
+   <br/>
+  <br/>
+  Endereço: <input type="text" name="Endereço"/>
+  Cidade: <input type="text" name="Cidade"/>
+  <br/>
+  <br/>
+  Estado: <select name="Estado" >
+    <option value="ac">Acre</option>
+    <option value="al">Alagoas</option>
+    <option value="am">Amazonas</option>
+    <option value="ap">Amapá</option>
+    <option value="ba">Bahia</option>
+    <option value="ce">Ceará</option>
+    <option value="df">Distrito Federal</option>
+    <option value="es">Espírito Santo</option>
+    <option value="go">Goiás</option>
+    <option value="ma">Maranhão</option>
+    <option value="mt">Mato Grosso</option>
+    <option value="ms">Mato Grosso do Sul</option>
+    <option value="mg">Minas Gerais</option>
+    <option value="pa">Pará</option>
+    <option value="pb">Paraíba</option>
+    <option value="pr">Paraná</option>
+    <option value="pe">Pernambuco</option>
+    <option value="pi">Piauí</option>
+    <option value="rj">Rio de Janeiro</option>
+    <option value="rn">Rio Grande do Norte</option>
+    <option value="ro">Rondônia</option>
+    <option value="rs">Rio Grande do Sul</option>
+    <option value="rr">Roraima</option>
+    <option value="sc">Santa Catarina</option>
+    <option value="se">Sergipe</option>
+    <option value="sp">São Paulo</option>
+    <option value="to">Tocantins</option>
+ 
+</select>
+
+  Bairro: <input type="text" name="Bairro"/>
+  <br/>
+  <br/>
+  Distrito: <input type="text" name="Distrito"/>
+  Etnia:
+   <select name="Étnia" id="Étnia">
+    <option value="Branco">Branco</option>
+    <option value="Pardo">Pardo</option>
+    <option value="Negros">Negros</option>
+    <option value="Indígena">Indígena</option>
+    <option value="Amarelo">Amarelo</option>
+    <option value="Branco">Branco</option>
+    <option value="Mulato">Mulato</option>
+    <option value="Caboclo">Caboclo</option>
+    <option value="Cafuzo">Cafuzo</option>
+</select>
+         <br/>
+     <br/>
+  <input type="submit" value="Cadastrar" />
+  </form>
+</body>
+</html>
