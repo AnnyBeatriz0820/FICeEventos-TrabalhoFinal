64705af246b899b1ce6bb5a3b830d16261bacf4d
nilt0on <54370415+nilt0on@users.noreply.github.com>
2019-10-29 16:41:00 -0300

    Mesa Redonda
    Pagina de Inscrição

---
+++ mesaredonda.php
@@ -0,0 +1,60 @@
+<html>
+<body>
+  </div>
+       <h1> <center> Inscrições para Mesa Redonda </center> </h1>
+     
+
+     
+      <div>
+<br/>
+<form method="post" action="Iniciação-Cientifica.php">
+   <form>
+  <div class="form-row">
+    <div class="Nome">
+      <label for="validationServer01">Nome Completo:</label>
+      <input type="text" class="form-control is-valid" id="validationServer01" placeholder="Nome Completo" value="Nome" required>
+      <div class="valid-feedback">
+      </div>
+    </div>
+    
+  <div class="form-row">
+    <div class="Cidade">
+      <label for="validationServer03">Cidade:</label>
+      <input type="text" class="form-control is-invalid" id="validationServer03" placeholder="Cidade" required>
+      <div class="invalid-feedback">
+       
+      </div>
+    </div>
+    <div class="Estado">
+      <label for="validationServer04">Estado:</label>
+      <input type="text" class="form-control is-invalid" id="validationServer04" placeholder="Estado" required>
+      <div class="invalid-feedback">
+      </div>
+    </div>
+    <div class="E-mail">
+      <label for="validationServer05">E-mail / CPF:</label>
+      <input type="text" class="form-control is-invalid" id="validationServer05" placeholder="E-Mail" required>
+      <div class="invalid-feedback">
+     <div>
+  </div>
+  <div class="form-group">
+    <div class="form-check">
+      <input class="form-check-input is-invalid" type="checkbox" value="" id="invalidCheck3" required>
+      <label class="form-check-label" for="invalidCheck3">
+        Aceito os Termos.
+      </label>
+      <div class="invalid-feedback">
+      </div>
+    </div>
+  </div>
+  <button class="btn btn-primary" type="submit">Enviar Inscrição</button>
+</form>
+  
+  <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+  <script src="./bootstrap/js/./bootstrap.min.js"></script>
+  <script src="https:/ajax.googleaois.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+ 
+
+  </form>
+</body>
+</html>
