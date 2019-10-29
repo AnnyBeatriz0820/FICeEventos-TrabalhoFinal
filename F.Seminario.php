d063dcaa7e11d6644d9abd1c73e1f7ae82089fe0
rogeriols79 <54370190+rogeriols79@users.noreply.github.com>
2019-10-29 16:55:07 -0300

    Tela Para Inscrições em Seminário
    Formulário para o Aluno  se inscrever em Seminário.

---
+++ F.Seminario.php
@@ -0,0 +1,110 @@
+<html>
+<body STYLE="text-align:center";>
+  <div>
+   <fieldset>
+       <h1 STYLE="background-color:beige";>Inscrição Para Seminário</h1>
+     
+      <div>
+
+<br/>
+<form method="post" action="F.Seminario.php">
+   <!--Curso: <select name="Curso" >
+
+    <option value="Ingles">Ingles</option>
+    <option value="Libras">Libras</option>
+    <option value="Matematica">Matematica</option>
+    <option value="Espanhol">Espanhol</option>
+    <option value="Programacao">Programaçao</option>
+    <option value="Introdução a Computacao">Introdução a Computacao</option>
+    <option value="Meio Ambiente">Meio Ambiente</option>
+    <option value="es">Informatica</option>
+     
+</select>
+ Matrícula: <input type="text" name="Nome" />
+ <br/>-->
+ <fieldset>
+  Nome Completo: <input type="text" name="Nome" />
+  </fieldset>
+   <br/>
+  <fieldset>
+  Email: <input type="text" name="Email"/>
+   
+   &nbsp &nbsp &nbsp &nbsp &nbsp Telefone: <input type="tel" name="Telefone"/>
+   </fieldset>
+    <br/>
+<!--CPF/Passaporte<input type="text" id="numero" placeholder="000.000.000-00">
+<br/>
+Nº RG<input type="text" id="numero" placeholder="0000000000000">
+ <br/>-->
+ <fieldset>
+   Sexo:
+   <input type="radio" name="Sexo" value="Masculino">M
+   <input type="radio" name="Sexo" value="Feminino">F
+   <input type="radio" name="Sexo" value="Outros">Outros
+   Data de Nascimento: <input type="date" name="data"/>
+   </fieldset>
+    <br/>
+   <fieldset>
+  Endereço: <input type="text" name="Endereço"/>
+ &nbsp &nbsp &nbsp Cidade: <input type="text" name="Cidade"/>
+  </fieldset>
+   <br/>
+  <fieldset>
+  Estado: <select name="Estado" >
+    <option value="ac">Acre</option>
+    <option value="al">Alagoas</option>
+    <option value="am">Amazonas</option>
+    <option value="ap">Amapá</option>
+    <option value="ba">Bahia</option>
+    <option value="ce">Ceará</option>
+    <option value="df">Distrito Federal</option>
+    <option value="es">Espírito Santo</option>
+    <option value="go">Goiás</option>
+    <option value="ma">Maranhão</option>
+    <option value="mt">Mato Grosso</option>
+    <option value="ms">Mato Grosso do Sul</option>
+    <option value="mg">Minas Gerais</option>
+    <option value="pa">Pará</option>
+    <option value="pb">Paraíba</option>
+    <option value="pr">Paraná</option>
+    <option value="pe">Pernambuco</option>
+    <option value="pi">Piauí</option>
+    <option value="rj">Rio de Janeiro</option>
+    <option value="rn">Rio Grande do Norte</option>
+    <option value="ro">Rondônia</option>
+    <option value="rs">Rio Grande do Sul</option>
+    <option value="rr">Roraima</option>
+    <option value="sc">Santa Catarina</option>
+    <option value="se">Sergipe</option>
+    <option value="sp">São Paulo</option>
+    <option value="to">Tocantins</option>
+    </select>
+
+  &nbsp &nbsp &nbsp &nbsp &nbspBairro: <input type="text" name="Bairro"/>
+  </fieldset>
+  <br/>
+  <fieldset>
+  Distrito: <input type="text" name="Distrito"/>
+  &nbsp &nbsp &nbsp &nbsp &nbspEtnia:
+   <select name="Étnia" id="Étnia">
+    <option value="Branco">Branco</option>
+    <option value="Pardo">Pardo</option>
+    <option value="Negros">Negros</option>
+    <option value="Indígena">Indígena</option>
+    <option value="Amarelo">Amarelo</option>
+    <option value="Branco">Branco</option>
+    <option value="Mulato">Mulato</option>
+    <option value="Caboclo">Caboclo</option>
+    <option value="Cafuzo">Cafuzo</option>
+</select>
+         </fieldset>
+     <br/>
+     <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+  <script src="./bootstrap/js/./bootstrap.min.js"></script>
+  <script src="https:/ajax.googleaois.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <h3 STYLE="background-color:beige";><input type="submit" value="INSCREVER" /></h3>
+  </form>
+  </fieldset>
+</div>
+</body>
+</html>
