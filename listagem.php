254ae0d90ddea2f69966c08100fe03eb47533bb3
AntoniaMeliana <54639588+AntoniaMeliana@users.noreply.github.com>
2019-11-05 16:56:46 -0300

    página listagem
    listagem com bootstrap

---
+++ listagem.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>listagem</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>Lista de Inscritos</h2>
+              
+  <table class="table table-condensed">
+    <thead>
+      <tr>
+        <th>Nome Completo</th>
+        <th>CPF</th>
+        <th>Email</th>
+        <th>Endereço</th>
+        <th>Curso / Evento</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John Silva</td>
+        <td>256982123-40</td>
+        <td>john@example.com</td>
+        <td>rua dos bodes</td>
+        <td>inforday</td>
+      </tr>
+      <tr>
+        <td>Mary Alexandre</td>
+        <td>546255965-30</td>
+        <td>mary@example.com</td>
+        <td>rua do João dos Queijos</td>
+        <td>inforday</td>
+      </tr>
+      <tr>
+        <td>July Juliana</td>
+        <td>855269561-30</td>
+        <td>july@example.com</td>
+      <td>rua joão viana</td>
+      <td>curso espanhol</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+</body>
+</html>
