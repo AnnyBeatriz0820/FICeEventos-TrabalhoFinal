1a0d4e540479014f27e4c837a278a0705950a782
Rogério <srogerio910@gmail.com>
2019-11-19 17:59:17 -0200

    Alteração no código da tabela.
    foi alterado os dados do arquivo tables.php.

--- tables.php
+++ tables.php
@@ -16,8 +16,8 @@
         <div class='container-fluid'>
 
            <!-- Page Heading -->
-           <h1 class="h3 mb-2 text-gray-800">Tables</h1>
-          <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the <a target="_blank" href="https://datatables.net">official DataTables documentation</a>.</p>
+           <h1 class="h3 mb-2 text-gray-800">Tabelas com dados do Evento</h1>
+          
 
           <!-- PARTE PRINCIPAL DA PAGINA ONDE DEVE SER ADICIONADO O CONTEUDO-->
 
@@ -25,19 +25,19 @@
           <!-- DataTales Example -->
           <div class="card shadow mb-4">
             <div class="card-header py-3">
-              <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+              <h6 class="m-0 font-weight-bold text-primary">Dados da Tabela</h6>
             </div>
             <div class="card-body">
               <div class="table-responsive">
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                   <thead>
                     <tr>
-                      <th>Name</th>
-                      <th>Position</th>
-                      <th>Office</th>
-                      <th>Age</th>
-                      <th>Start date</th>
-                      <th>Salary</th>
+                      <th>Inscrição</th>
+                      <th>Nome</th>
+                      <th>Ação</th>
+                      <th>Carga Horária</th>
+                      <th>Data</th>
+                      <th>Email</th>
                     </tr>
                   </thead>
                   <tfoot>
