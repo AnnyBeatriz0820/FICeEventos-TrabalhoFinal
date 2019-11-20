9402843d1743d47650f90bf215825875d9e8ad02
Beatriz <Beatrizhranny28@gmail.com>
2019-11-20 14:58:55 -0300

    Adicionando Arquivos  TELA DE LOGIN
    Tela Login, Esqueceu Senha e Cadastro

--- template/menu-lateral.php
+++ template/menu-lateral.php
@@ -35,7 +35,7 @@ echo "
       <li class='nav-item'>
         <a class='nav-link collapsed' href='#' data-toggle='collapse' data-target='#collapseUtilities' aria-expanded='true' aria-controls='collapseUtilities'>
           <i class='fas fa-fw fa-folder'></i>
-          <span>Ações</span>
+          <span>Ações Usuários</span>
         </a>
         <div id='collapseUtilities' class='collapse' aria-labelledby='headingUtilities' data-parent='#accordionSidebar'>
           <div class='bg-white py-2 collapse-inner rounded'>
