90f7e39ae21d35eceaa45ded048baaad10ad3b88
Italo <italo.arruda@ifce.edu.br>
2019-12-03 00:52:04 -0300

    Reformulação da Estrutura do Projeto
    Definido os eventos principais
    Redirecionando para a pagina de Adm ou usuario
    Fazendo o login funcionar parcialmente
    Resolução de conflitos existens
    Montagem da estrutura de conexao com o BD

---
+++ template/carrega-table.php
@@ -0,0 +1,113 @@
+
+
+<thead>
+  <tr>
+    <th>Nome</th>
+    <th>E-mail</th>
+    <th>Idade</th>
+    <th>Início</th>
+    <th>Evento</th>
+    <th>Situação</th>
+  </tr>
+</thead>
+<tfoot>
+  <tr>
+    <th>Nome</th>
+    <th>E-mail</th>
+    <th>Idade</th>
+    <th>Início</th>
+    <th>Evento</th>
+    <th>Situação</th>
+  </tr>
+</tfoot>
+<tbody>
+
+  <?php 
+
+
+  include './funcoes/conn.php';
+
+
+
+  $sql = "SELECT * FROM usuario ";
+
+//  $resultado = $conn->query($sql);
+
+  if ($resultado->num_rows > 0) {
+    while($linha = $resultado->fetch_assoc()) {
+
+      $id             = $linha["id"];
+      $nome           = $linha["nome"];
+      $data           = $linha["data_de_nascimento"];
+      $email          = $linha["email"];
+      $CPF            = $linha["cpf"];
+      $data_cadastro  = $linha["data_cadastro"];
+      $situacao       = $linha["situacao"];
+      $tipo           = $linha["tipo"];
+
+
+
+
+      $idade = calculo_idade($data);
+
+
+      if($tipo == 2){
+
+        ?>
+
+        <tr>
+          <td><?php echo $nome; ?></td>
+          <td><?php echo $email; ?></td>
+          <td><?php echo $idade; ?></td>
+          <td><?php echo $data_cadastro; ?></td>
+          <td></td>
+          <td><?php echo $situacao; ?></td>
+        </tr>
+
+        <?php  
+      }
+    }
+  } else {  ?>
+  <tr >
+    <td colspan=6 align="center">Sem Registros no Banco ou select ERRADO!!!</td>
+  </tr>
+
+<?php  }
+
+  function calculo_idade($data) {
+        //Data atual
+    $dia = date('d');
+    $mes = date('m');
+    $ano = date('Y');
+        //Data do aniversário
+        /*
+        $nascimento = explode('/', $data);
+        $dianasc = ($nascimento[0]);
+        $mesnasc = ($nascimento[1]);
+        $anonasc = ($nascimento[2]);
+        */
+        // se for formato do banco, use esse código em vez do de cima!
+        
+        $nascimento = explode('-', $data);
+        $dianasc = ($nascimento[2]);
+        $mesnasc = ($nascimento[1]);
+        $anonasc = ($nascimento[0]);
+        
+        //Calculando sua idade
+        $idade = $ano - $anonasc; // simples, ano- nascimento!
+        if ($mes < $mesnasc){
+          $idade--;
+          return $idade;
+        }elseif ($mes == $mesnasc && $dia <= $dianasc){
+          $idade--;
+          return $idade;
+        }else{
+          return $idade;
+        }
+      } 
+
+      ?>
+
+
+    </tbody>
+
