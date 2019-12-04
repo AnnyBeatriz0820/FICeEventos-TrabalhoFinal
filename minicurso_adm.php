dd51863f242e9bf1024d387005dbfce1b748d127
Beatriz <Beatrizhranny28@gmail.com>
2019-12-04 15:11:56 -0300

    Adicionando Oficina, MiniCurso, FIC
    Arquivos adm e usuario de Oficina e MiniCurso e FIC

--- minicurso_adm.php
+++ minicurso_adm.php
@@ -1,41 +1,4 @@
-
-<!DOCTYPE html>
-<html lang='en'>
-  
- <?php include 'template/cabecalho.php' ?>
- <?php include 'template/menu-lateral.php' ?>
-
-    <!-- Content Wrapper -->
-    <div id='content-wrapper' class='d-flex flex-column'>
-
-      <!-- Main Content -->
-      <div id='content'>
-
-      <?php include 'template/menu-superior.php' ?>       
-
-        <!-- Begin Page Content -->
-        <div class='container-fluid'>
-
-          <!-- Page Heading -->
-          <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800"></h1>
-            
-          </div>
-          <style>
-
-    h11 {
-      color:red;
-  }
-  #logo {
-          width:50%;
-          height:50%;
-  }
-  .panel-heading{
-      font-size:150%;
-  }
-  </style>
-          <!-- PARTE PRINCIPAL DA PAGINA ONDE DEVE SER ADICIONADO O CONTEUDO-->
-          <div class="container">
+<div class="container">
 <form action="index.php" method="post">
    <div>
         <fieldset>
@@ -233,8 +196,6 @@
             <option value="Umirim">Campus Umirim</option>
     </select>
   </div>
-
- 
 </div>
  <fieldset>
 <!-- Button (Double) -->
@@ -248,32 +209,3 @@
 </fieldset>
 </form>
 </div>
-          <!-- FIM PARTE PRINCIPAL DA PAGINA ONDE DEVE SER ADICIONADO O CONTEUDO-->
-
-          </div>
-        <!-- /.container-fluid -->
-
-      </div>
-      <!-- End of Main Content -->
-
-      <?php include 'template/rodape.php' ?>
-    
-
-    </div>
-    <!-- End of Content Wrapper -->
-
-  </div>
-  <!-- End of Page Wrapper -->
-
-  <!-- Scroll to Top Button-->
-  <a class='scroll-to-top rounded' href='#page-top'>
-    <i class='fas fa-angle-up'></i>
-  </a>
-
-  <?php include 'template/logout.php' ?>
-
-  <?php include 'template/imports.php' ?>
-
-</body>
-
-</html>
--- minicurso_usuario.php
+++ minicurso_usuario.php
@@ -1,51 +1,8 @@
-<!DOCTYPE html>
-<html lang='en'>
+<div class="container">
 
-<?php include 'template/cabecalho.php' ?>
-<?php include 'template/menu-lateral.php' ?>
-
-<!-- Content Wrapper -->
-<div id='content-wrapper' class='d-flex flex-column'>
-
-	<!-- Main Content -->
-	<div id='content'>
-
-		<?php include 'template/menu-superior.php' ?>       
-
-		<!-- Begin Page Content -->
-		<div class='container-fluid'>
-
-			<!-- Page Heading -->
-			<div class="d-sm-flex align-items-center justify-content-between mb-4">
-				<h1 class="h3 mb-0 text-gray-800"></h1>
-				
-			</div>
-			<style>
-
-    h11 {
-      color:red;
-  }
-  #logo {
-          width:50%;
-          height:50%;
-  }
-  .panel-heading{
-      font-size:150%;
-  }
-  </style>
-			<!-- PARTE PRINCIPAL DA PAGINA ONDE DEVE SER ADICIONADO O CONTEUDO-->
-
-			<div class="container">
-				<form class="form-horizontal" role="form" method="POST" action="index.php">
-
-<<<<<<< HEAD
-						<b>
-							<div>
-=======
-					<div>
->>>>>>> master
+          <form method="post" action="index.php">
+  <div>
         <fieldset>
-        
        <h1 STYLE="text-align:center;"> <IMG SRC="https://2.bp.blogspot.com/-QYgmjjRaouk/TkPusQrEGxI/AAAAAAAABIE/JlTLuECoQYc/s300/ifce.jpg"></h1> 
        
         <div STYLE="text-align:center;">
@@ -260,49 +217,6 @@
   </div>
 </div>
 </fieldset>
-</form>
-<<<<<<< HEAD
-					<div class="row">
-						<div class="col-md-3"></div>
-						<div class="col-md-6">
-							<button type="submit" class="btn btn-success"><i class="fa fa-user-plus"></i> Register</button>
-						</div>
-					</div>
-				</form>
-=======
->>>>>>> master
-			</div>
-			
-			
-			
-			
-
-			<!-- FIM PARTE PRINCIPAL DA PAGINA ONDE DEVE SER ADICIONADO O CONTEUDO-->
-
-		</div>
-		<!-- /.container-fluid -->
-
-	</div>
-	<!-- End of Main Content -->
-
-	<?php include 'template/rodape.php' ?>
-	
-
+</form>       
 </div>
-<!-- End of Content Wrapper -->
-
-</div>
-<!-- End of Page Wrapper -->
-
-<!-- Scroll to Top Button-->
-<a class='scroll-to-top rounded' href='#page-top'>
-	<i class='fas fa-angle-up'></i>
-</a>
-
-<?php include 'template/logout.php' ?>
-
-<?php include 'template/imports.php' ?>
-
-</body>
-
-</html>
+          
