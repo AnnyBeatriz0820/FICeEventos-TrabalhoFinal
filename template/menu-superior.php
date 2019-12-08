cada45bc85bd45329e03316e24726d6ac04a51bb
Italo <italo.arruda@ifce.edu.br>
2019-12-08 03:11:12 -0300

    Ocultando a barra lateral
    Botão adicionado

--- template/menu-superior.php
+++ template/menu-superior.php
@@ -4,7 +4,10 @@ echo "
 <!-- Topbar -->
         <nav class='navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow'>
 
-          
+        <button class='btn btn-link btn-sm text-black order-1 order-sm-0' id='sidebarToggle' href='#'>
+      <i class='fas fa-bars'></i>
+    </button>  
+        
             <div class='input-group'>
               
               <div class='input-group-append'>
@@ -19,9 +22,6 @@ echo "
 
             <!-- Nav Item - Search Dropdown (Visible Only XS) -->
             <li class='nav-item dropdown no-arrow d-sm-none'>
-              <a class='nav-link dropdown-toggle' href='#' id='searchDropdown' role='button' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
-                <i class='fas fa-search fa-fw'></i>
-              </a>
               <!-- Dropdown - Messages -->
               <div class='dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in' aria-labelledby='searchDropdown'>
                 <form class='form-inline mr-auto w-100 navbar-search'>
