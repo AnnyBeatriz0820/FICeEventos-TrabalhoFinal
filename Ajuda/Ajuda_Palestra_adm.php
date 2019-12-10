cd2e79e00b8faf0a34c7cbb6d59a945b824becbb
Beatriz <Beatrizhranny28@gmail.com>
2019-12-10 16:57:01 -0300

    Tela de ajuda Index, PalestraADM e Register
    Adicionando arquivos.

---
+++ Ajuda/Ajuda_Palestra_adm.php
@@ -0,0 +1,62 @@
+<html lang="pt-br">
+  <head>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+ 
+    <title>Modalt</title>
+  </head>
+  <body>
+    <!-- Botão que irá abrir o modal -->
+    <div class=text-center>
+  <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#meuModal">?</button>
+ </div>
+    <!-- Modal -->
+    <div id="meuModal" class="modal fade" role="dialog">
+      <div class="modal-dialog">
+ 
+        <!-- Conteúdo do modal-->
+        <div class="modal-content">
+ 
+          <!-- Cabeçalho do modal -->
+          <div class="modal-header">
+            <h4 class="modal-title">Inserindo Informações</h4>
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+          </div>
+ 
+          <!-- Corpo do modal -->
+          <div class="modal-body">
+           <p style="color:red";>
+              Siga as instruções abaixo!
+              </p>
+            <p>1º Insira todas as informações necessárias para Palestra!</p>
+            <p>2º Nome do Palestrante</p>
+            <p>3º Quantidades de Vagas</p>
+             <p>4º Data da Palestra</p>
+             <p>5º Público Alvo </p>
+             <p>6º Telefone da Cordenação da Palestra</p>
+             <p>7º Telefone do Campus</p>
+             <p>8º Email para dúvidas sobre a Palestra</p>
+             <p>9º Programação</p>
+             <p>10º Selecione o Campus que vai Realizar a Palestra </p>
+            <p>11º Clique em Inscrever.</p>
+            <p style="color:green";>
+          Após seguir os passos descritos acima corretamente,todos os dados serão inseridos com sucesso!
+        </p>
+          </div>
+ 
+          <!-- Rodapé do modal-->
+          <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-dismiss="modal">Fechar</button>
+          </div>
+ 
+        </div>
+      </div>
+    </div>
+ 
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ Ajuda/Ajuda_register.php
@@ -0,0 +1,65 @@
+<html lang="pt-br">
+  <head>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Modalt</title>
+  </head>
+  <body>
+    <!-- Botão que irá abrir o modal -->
+    <div class=text-center>
+  <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#meuModal">?</button>
+ </div>
+    <!-- Modal -->
+    <div id="meuModal" class="modal fade" role="dialog">
+      <div class="modal-dialog">
+ 
+        <!-- Conteúdo do modal-->
+        <div class="modal-content">
+ 
+          <!-- Cabeçalho do modal -->
+          <div class="modal-header">
+            <h4 class="modal-title">Registrando Dados</h4>
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+          </div>
+ 
+          <!-- Corpo do modal -->
+          <div class="modal-body">
+           <p style="color:red";>
+              Siga as instruções abaixo!
+              </p>
+            <p>1ºInsira todas as informações necessárias para Registra-se no site!</p>
+            <p>2ºNome completo</p>
+            <p>3ºSexo</p>
+             <p>4ºCPF</p>
+             <p>5ºData Nascimento </p>
+             <p>6ºTelefone</p>
+             <p>7ºEmail</p>
+             <p>8ºCEP</p>
+             <p>9ºEstado</p>
+             <p>10ºCidade</p>
+            <p>11ºRua</p>
+            <p>12ºNúmero</p>
+            <p>13ºBairro</p>
+            <p>14ºComplemento</p>
+            <p>15º Clique em Cadastrar.</p>
+            <p style="color:green";>
+          Após seguir os passos descritos acima corretamente,você será Cadastrado com sucesso!
+        </p>
+          </div>
+ 
+          <!-- Rodapé do modal-->
+          <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-dismiss="modal">Fechar</button>
+          </div>
+ 
+        </div>
+      </div>
+    </div>
+ 
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
