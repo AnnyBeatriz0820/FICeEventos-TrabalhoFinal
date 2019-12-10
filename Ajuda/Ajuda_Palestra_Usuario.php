54e0bd13ee2f4b27d72effbc60009f5b7010e698
Beatriz <Beatrizhranny28@gmail.com>
2019-12-10 10:14:15 -0300

    Tela ajuda Palestra_Usuario
    Adicionando tela de ajuda.

---
+++ Ajuda/Ajuda_Palestra_Usuario.php
@@ -0,0 +1,55 @@
+<html lang="pt-br">
+  <head>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+ 
+    <title>Modalt</title>
+  </head>
+  <body>
+    <!-- Botão que irá abrir o modal -->
+    <div class=text-center>
+  <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#meuModal">Ajuda</button>
+ </div>
+    <!-- Modal -->
+    <div id="meuModal" class="modal fade" role="dialog">
+      <div class="modal-dialog">
+ 
+        <!-- Conteúdo do modal-->
+        <div class="modal-content">
+ 
+          <!-- Cabeçalho do modal -->
+          <div class="modal-header">
+            <h4 class="modal-title">Pronto (a) para realizar sua inscrição?</h4>
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+          </div>
+ 
+          <!-- Corpo do modal -->
+          <div class="modal-body">
+           <p style="color:red";>
+              Siga os passos abaixo!
+              </p>
+            <p>1º Descreva a Palestra que deseja participar.</p>
+            <p>2º Clique em Inscrever.</p>
+            <p style="color:green";>
+          Após seguir os passos descritos acima, sua inscrição será realizada com sucesso!
+        </p>
+          </div>
+ 
+          <!-- Rodapé do modal-->
+          <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-dismiss="modal">Fechar</button>
+          </div>
+ 
+        </div>
+      </div>
+    </div>
+ 
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
