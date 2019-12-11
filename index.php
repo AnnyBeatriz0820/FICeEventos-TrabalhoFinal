8fcf8416c6586c56a167d8c42b7bdb7eacae258b
nilt0on <54370415+nilt0on@users.noreply.github.com>
2019-12-11 14:16:24 -0300

    Index php
    seguindo o modelo de congresso_adm só com o form

--- index.php
+++ index.php
@@ -1,55 +1,7 @@
-<?php
-// remove all session variables
-session_unset();
 
-// destroy the session
-session_destroy();
-
-print_r($_SESSION);
-?>
-
-<!DOCTYPE html>
-<html lang="pt">
-
-<head>
-
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>IFCE Campus Acopiara</title>
-
-  <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-  <!-- Custom styles for this template-->
-  <link href="css/sb-admin-2.min.css" rel="stylesheet">
-
-</head>
-
-<body class="bg-gradient-primary">
-
-  <div class="container">
-
-    <!-- Outer Row -->
-    <div class="row justify-content-center">
-
-      <div class="col-xl-10 col-lg-12 col-md-9">
-
-        <div class="card o-hidden border-0 shadow-lg my-5">
-          <div class="card-body p-0">
-            <!-- Nested Row within Card Body -->
-            <div class="row">
-              <div ></div>
-              <div class="col-lg-6">
-                <div class="p-5">
-                  <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">Bem vindo de volta!</h1>
+                    <h1 class="h4 text-gray-900 mb-4">Bem-vindo de volta!</h1>
                   </div>
-                  <form id="loginForm" class="user" method="POST" action="main.php">
+                  <form id="loginForm" class="user was-validated" method="POST" action="main.php">
                     <div class="form-group">
                       <input type="email" class="form-control form-control-user" id="email" name="email" aria-describedby="emailHelp" placeholder="Email...">
                     </div>
@@ -69,35 +21,4 @@ print_r($_SESSION);
                     </div>
                     <hr>
                   </form>
-                  <hr>
-                  <div class="text-center">
-                    <a class="small" href="forgot-password.php">Esqueceu a senha?</a>
-                  </div>
-                  <div class="text-center">
-                    <a class="small" href="register.php">Crie a sua conta aqui!</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-      </div>
-
-    </div>
-
-  </div>
-
-  <!-- Bootstrap core JavaScript-->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Core plugin JavaScript-->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-  <!-- Custom scripts for all pages-->
-  <script src="js/sb-admin-2.min.js"></script>
-
-</body>
-
-</html>
+                 
